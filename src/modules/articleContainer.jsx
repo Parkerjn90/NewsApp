3d25c2820ef55b514bd21233ed5b248f81fc340a
Joy <parkerjn90@gmail.com>
2023-05-15 18:04:40 -0400

    add article card to article container

--- src/modules/articleContainer.jsx
+++ src/modules/articleContainer.jsx
@@ -1,4 +1,5 @@
 
+import Article from './articles.jsx'
 
 const ArticleContainer = () => {
 
---
+++ src/modules/articles.jsx
@@ -0,0 +1,10 @@
+
+
+const Article = () => {
+
+  return (
+    <></>
+  );
+};
+
+export default Article;
